de9006832e34306db0712ae52d24df5f3f3829cb
BGal23 <131159103+BGal23@users.noreply.github.com>
2023-08-15 01:03:08 +0200

    2 finish

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,30 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const fotoList = document.querySelector(".gallery");
+
+const addFotoList = galleryItems.map(foto => `
+    <li class="gallery__item">
+        <a class="gallery__link"
+        href=${foto.original}>
+            <img class="gallery__image"
+            src=${foto.preview} 
+            alt=${foto.description}/>
+        </a>
+    </li>
+`).join(" ");
+
+fotoList.innerHTML = addFotoList;
+
+
+let modalFoto = new SimpleLightbox('.gallery__link', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    });
+
+document.addEventListener("keydown", event => {
+    if (event.key === "Escape") {
+        modalFoto.close()
+    }
+});
