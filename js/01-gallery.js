d0850e3f0013cf84c048a326ab2d5cd01d9f2370
BGal23 <131159103+BGal23@users.noreply.github.com>
2023-08-14 13:45:09 +0200

    f

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -7,21 +7,10 @@ const fotoList = document.querySelector(".gallery");
 
 const newFotoList = galleryItems.map(foto => `
     <li>
-        <div class="gallery__item">
-            <a class="gallery__link">
-                <img class="gallery__image" src=${foto.preview} alt=${foto.preview}/>
-            </a>
-        </div>
+        <a class="gallery__link">
+            <img class="gallery__image" src=${foto.preview} alt=${foto.preview}/>
+        </a>
     </li>
 `).join(" ")
 
 fotoList.innerHTML = newFotoList
-
-
-document.querySelector('.gallery__image').addEventListener("click", () => {
-console.log(galleryItems[1].original)
-const instance = basicLightbox.create(`
-    <img src="${galleryItems[1].original}" >`)
-
-    instance.show()
-});
